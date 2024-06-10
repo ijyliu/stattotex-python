384ac56d830eb4a5644862e5dd94d3d5359dfca8
Isaac Liu <ijyliu@uchicago.edu>
2024-06-09 23:31:35 -0700

    fix version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setuptools.setup(
     name="stattotex",
     author="Isaac Liu",
     author_email="ijyliu@gmail.com",
-    release="0.1.1",
+    version="0.1.1",
     description="A simple function for automatically updating LaTeX documents with numbers from Python.",
     long_description=long_description,
     long_description_content_type="text/markdown",
